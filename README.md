04d1225d951596a6258290a104fc79d3c4823494
Alan Ning <askldjd@gmail.com>
2016-05-11 08:24:42 -0400

    Tested the Retry and TTL feature and updated documentation

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The library is only suitable for educational references, and should not be used 
 
 ## API
 
-## Creation
+### Creation
 `zookeeper-node-shared-lock` returns a factory function that creates a lock service. The
 function expects a set of configuration options. Once the service is created, you may subscribe to the `ready` event. Once the `ready` event is fired, you may begin locking/unlocking resources.
 
@@ -15,6 +15,8 @@ The configuration parameters includes the following:
  - `host` - The hostname or IP of the zookeeper service.
  - `port` - The port number of zookeeper service.
  - `resourceName` - The name of the resource. Internally, this will be used as the parent ephemeral znode that holds all the child nodes.
+ - `maxRetryCount` - The maximum number of times the lock will retry after request timeout. By default, this value is 0, which implies we will wait indefinitely for this resource.
+ - `initialRetryWaitMs` - The amount of time to wait after initial failure. As failures accumulates, the retry wait will increase using the [binary exponential backoff](https://en.wikipedia.org/wiki/Exponential_backoff) algorithm. This value has no effect if `maxRetryCount` is 0.
 
 #### Sample
 
@@ -37,19 +39,25 @@ lockService.events.on('ready', () => {
 });
 ```
 
-## lock(resourceId, ttl, [cb])
+### lock(resourceId, ttl, [cb])
 
 `lock` acquires a resource asynchronously. The `resourceId` is the shared resource to lock. If the shared resource is acquired, or failed to be acquired, the `cb(err, acquiredResource)` will be invoked.
 
 The return `acquiredResource` argument is used to `unlock` the acquired resource.
 
-If `ttl` (in milliseconds) is provided, the lock service will retry up to TTL before giving up. The retry algorithm is [binary exponential backoff](https://en.wikipedia.org/wiki/Exponential_backoff).
+If `ttl` (in milliseconds) is provided, the lock service will automatically release the lock upon TTL expiration. User may subscribe to the `ttlExpired` event for this notification.
 
-If `ttl` is 0 (default), lock service will wait indefinitely until the resource is acquired. There is no retry invoked since a permanent watch is used to monitor the resource.
+If `ttl` is 0 (default), the acquired lock resource will be held indefinitely.
 
-## unlock(acquiredResource, [cb])
+### unlock(acquiredResource, [cb])
 
 `unlock` releases a resource asynchronously. The `acquiredResource` is the acquired resource returned from `lock`.
+
+
+## Events
+
+- `ready` - fired when Lock Service is ready.
+- `ttlExpired` - fired with `function (resourceId)` if a resource is being forcefully released.
 
 ## ZooKeeper Lock Recipe
 
--- test/lock-service.test.js
+++ test/lock-service.test.js
@@ -4,7 +4,7 @@ const createLockService = require('../lib/lock-service');
 const RESOURCE_ID = 'myresource';
 const expect = require('expect');
 
-describe('', function() {
+describe('Lock Service test suite', function() {
 
   describe('Simple Lock Unlock Test', function() {
 
@@ -36,12 +36,35 @@ describe('', function() {
     });
   });
 
+  describe('Simple Lock with Unlock TTL Test', function() {
+
+    let lockService;
+    it('create lock service', function(done) {
+      let opt = {};
+      lockService = createLockService(opt);
+
+      lockService.events.on('ready', () => {
+        done();
+      });
+    });
+
+    it('lock a resource and wait for TTL expiration', function(done) {
+      lockService.events.on('ttlExpired', (resourceId) => {
+        expect(resourceId).toBeTruthy();
+        done();
+      });
+      lockService.lock(RESOURCE_ID, 500, (err, lockCtx) => {
+        expect(err).toBeFalsy();
+        expect(lockCtx).toBeTruthy();
+      });
+    });
+  });
+
   function makeTwoLockTest(opt) {
     return function() {
       let lockService;
       let acquiredResource;
       it('create lock service', function(done) {
-        let opt = {};
         lockService = createLockService(opt);
 
         lockService.events.on('ready', () => {
@@ -50,7 +73,7 @@ describe('', function() {
       });
 
       it('lock a resource', function(done) {
-        lockService.lock(RESOURCE_ID, opt.ttl, (err, resource) => {
+        lockService.lock(RESOURCE_ID, 0, (err, resource) => {
           acquiredResource = resource;
           done();
         });
@@ -59,7 +82,7 @@ describe('', function() {
       it('lock contention, need to wait 2 sec before acquiring', function(
         done) {
         this.timeout(10000);
-        lockService.lock(RESOURCE_ID, opt.ttl, (err, resource) => {
+        lockService.lock(RESOURCE_ID, 0, (err, resource) => {
           acquiredResource = resource;
           done();
         });
@@ -80,19 +103,22 @@ describe('', function() {
     };
   }
 
-  describe('Two-lock Test with infinite wait', makeTwoLockTest({
-    ttl: 0
+  describe('Two-lock Test with watch', makeTwoLockTest({
+    maxRetryCount: 0
   }));
 
-  describe('Two-lock Test with TTL', makeTwoLockTest({
-    ttl: 5000
-  }));
+  describe('Two-lock Test with periodic poll',
+    makeTwoLockTest({
+      maxRetryCount: 5000
+    }));
 
-  describe('Two-lock Test with TTL failure', function() {
+  describe('Two-lock Test with retry count failure', function() {
     let lockService;
     let acquiredResource;
     it('create lock service', function(done) {
-      let opt = {};
+      let opt = {
+        maxRetryCount: 1
+      };
       lockService = createLockService(opt);
 
       lockService.events.on('ready', () => {
@@ -101,7 +127,7 @@ describe('', function() {
     });
 
     it('lock a resource', function(done) {
-      lockService.lock(RESOURCE_ID, 500, (err, resource) => {
+      lockService.lock(RESOURCE_ID, 0, (err, resource) => {
         acquiredResource = resource;
         done();
       });
@@ -110,7 +136,7 @@ describe('', function() {
     it('lock contention, need to wait 2 sec before acquiring', function(
       done) {
       this.timeout(4000);
-      lockService.lock(RESOURCE_ID, 500, (err) => {
+      lockService.lock(RESOURCE_ID, 0, (err) => {
         expect(err).toBeTruthy();
         done();
       });
@@ -124,12 +150,12 @@ describe('', function() {
     });
   });
 
-  function makeNLockTest(opt) {
+  // N-Contenders for a single resource, and lock hold must be released manually.
+  function makeNLockTestWithManualTtl(opt) {
     return function() {
 
       let lockService;
       it('create lock service', function(done) {
-        let opt = {};
         lockService = createLockService(opt);
 
         lockService.events.on('ready', () => {
@@ -138,10 +164,10 @@ describe('', function() {
       });
 
       it('lock a resource', function(done) {
-        this.timeout(opt.lockHoldTimeMs * opt.numContention * 10);
+        this.timeout(opt.manualTtl * opt.numContention * 10);
         let acquired = 0;
         for (let i = 0; i < opt.numContention; ++i) {
-          lockService.lock(RESOURCE_ID, opt.ttl, (err, resource) => {
+          lockService.lock(RESOURCE_ID, 0, (err, resource) => {
             expect(err).toBeFalsy();
             ++acquired;
             setTimeout(() => {
@@ -151,24 +177,71 @@ describe('', function() {
                   done();
                 }
               });
-            }, opt.lockHoldTimeMs);
+            }, opt.manualTtl);
           });
         }
       });
     };
   }
 
+  // N-Contenders for a single resource, and lock hold will be released automatically
+  function makeNLockTestWithAutoTtl(opt) {
+    return function() {
 
-  describe('N-lock Test', makeNLockTest({
-    lockHoldTimeMs: 500,
-    numContention: 50,
-    ttl: 0
-  }));
+      let lockService;
+      it('create lock service', function(done) {
+        lockService = createLockService(opt);
 
-  describe('N-lock Test with TTL', makeNLockTest({
-    lockHoldTimeMs: 500,
-    numContention: 5,
-    ttl: 100000000
-  }));
+        lockService.events.on('ready', () => {
+          done();
+        });
+      });
 
+      it('lock a resource and wait for TTL expiration', function(done) {
+        this.timeout(opt.ttl * opt.numContention * 10);
+        let completed = 0;
+        lockService.events.on('ttlExpired', (resourceId) => {
+          expect(resourceId).toBeTruthy();
+          ++completed;
+          if (completed === opt.numContention) {
+            done();
+          }
+        });
+        for (let i = 0; i < opt.numContention; ++i) {
+          lockService.lock(RESOURCE_ID, opt.ttl, (err, lockCtx) => {
+            expect(err).toBeFalsy();
+            expect(lockCtx).toBeTruthy();
+          });
+        }
+      });
+    };
+  }
+
+  describe('N-lock Test with manual TTL and watch',
+    makeNLockTestWithManualTtl({
+      manualTtl: 500,
+      numContention: 50,
+      maxRetryCount: 0
+    }));
+
+  describe('N-lock Test with manual TTL and periodic poll',
+    makeNLockTestWithManualTtl({
+      manualTtl: 500,
+      numContention: 5,
+      maxRetryCount: 100000000
+    }));
+
+  describe('N-lock Test with auto TTL and watch',
+    makeNLockTestWithAutoTtl({
+      ttl: 500,
+      numContention: 50,
+      maxRetryCount: 0
+    }));
+
+  describe('N-lock Test with auto TTL and periodic poll',
+    makeNLockTestWithAutoTtl({
+      ttl: 500,
+      numContention: 5,
+      maxRetryCount: 100000000
+    }));
 });
